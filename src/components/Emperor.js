f5ed2e01a4f7176f7d455af8deb4a9f17912ff07
Alan-Miller <alan.ashman@gmail.com>
2018-01-30 06:23:29 -0700

    removed code from Emperor

--- src/components/Emperor.js
+++ src/components/Emperor.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { alterVaderAura, setEmpMsg, setLukeMsg } from '../reducer';
 
 function Emperor(props) {
   return (
-    <div className="Emperor" onClick={() => {
-      props.alterVaderAura(-20);
-      props.setEmpMsg('The Dark Side is awesome. Don\'t be a baby, Vader.');
-      props.setLukeMsg('');
-    }}>
+    <div className="Emperor">
       <h2>Emperor</h2>
     </div>
   )
 }
 
-export default connect(null, { alterVaderAura, setEmpMsg, setLukeMsg })(Emperor);
+export default Emperor;
