a3ba452619076e3d930dd9f2c8567c7bf26ffe58
Alan-Miller <alan.ashman@gmail.com>
2018-01-30 06:21:56 -0700

    removed code from reducer

--- src/reducer.js
+++ src/reducer.js
@@ -8,9 +8,8 @@ const initialState = {
 
 // DEFINE ACTIONS
 const SET_EMP_MSG = 'SET_EMP_MSG';
-const SET_LUKE_MSG = 'SET_LUKE_MSG';
-const ALTER_VADER_AURA = 'ALTER_VADER_AURA';
-
+// Luke action here
+// Vader action here
 
 // MAKE ACTION CREATORS TO SEND ACTIONS TO REDUCER
 export function setEmpMsg(msg) {
@@ -19,34 +18,17 @@ export function setEmpMsg(msg) {
     payload: msg
   }
 }
-export function setLukeMsg(msg) {
-  return {
-    type: SET_LUKE_MSG,
-    payload: msg
-  }
-}
-export function alterVaderAura(influence) {
-  return {
-    type: ALTER_VADER_AURA,
-    payload: influence
-  }
-}
 
+// Luke action creator here 
+// Vader action creator here
 
 // MAKE REDUCER FUNCTION WHICH WILL MAKES CHANGES TO STATE
-export default function (state = initialState, action) {
-  switch (action.type) {
 
-    case SET_EMP_MSG:
-      return Object.assign({}, state, { emperorsMessage: action.payload });
-    case SET_LUKE_MSG:
-      return Object.assign({}, state, { lukesMessage: action.payload });
-    case ALTER_VADER_AURA:
-      let aura = +state.vadersAura + +action.payload;
-      aura = aura > 255 ? 255 : aura < 0 ? 0 : aura;
-      return Object.assign({}, state, { vadersAura: aura.toString() });
 
-    default:
-      return state;
-  }
-}
+
+
+
+// NOTES FOR AURA CASE
+// let aura = +state.vadersAura + +action.payload;
+// aura = aura > 255 ? 255 : aura < 0 ? 0 : aura;
+// return Object.assign({}, state, { vadersAura: aura.toString() });
