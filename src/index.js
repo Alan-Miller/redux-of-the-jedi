82cc18bde48992b6a64dc419bb937dd5c0bc56e3
Alan-Miller <alan.ashman@gmail.com>
2018-01-30 06:27:37 -0700

    deleted unsed images and removed code from store and index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { unregister } from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import store from './store';
+// import { Provider } from 'react-redux';
+// import store from './store';
 import './styles/reset.css';
 import './styles/main.css';
 
 ReactDOM.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <App />
-  </Provider>
+  // </Provider>
   , document.getElementById('root'));
 unregister();
