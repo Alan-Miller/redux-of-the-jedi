f0623f49c71dbc466ba3800f37ceea28a224ea21
Alan-Miller <alan.ashman@gmail.com>
2018-04-03 15:43:39 -0600

    fixed Vader color

--- src/components/Vader.js
+++ src/components/Vader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Vader(props) {
 
   return (
-    <div className="Vader" style={{ borderColor: `rgb(${aura}, ${aura}, ${aura})` }}>
+    <div className="Vader" style={{ borderColor: `rgb(0, 0, 0)` }}>
       {/* <h1>Vader</h1> */}
     </div>
   )
